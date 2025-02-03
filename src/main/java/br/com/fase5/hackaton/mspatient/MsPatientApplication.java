4d1e2cfc72e5b68a6e536c2b52151b0bb56db258
Adriano Pinheiro <adriano_fiorini@hotmail.com>
2025-02-02 21:04:14 -0300

    Estrutura do MicrosServiço de Pacientes

---
+++ src/main/java/br/com/fase5/hackaton/mspatient/MsPatientApplication.java
@@ -0,0 +1,13 @@
+package br.com.fase5.hackaton.mspatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fase5/hackaton/mspatient/MsPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fase5.hackaton.mspatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
